6ded8f7199af13cdb43d5b37b14c407f5703be15
Ogie Galicia <o.galicia.cpa@gmail.com>
2025-11-05 15:58:29 +0800

    fix: change framework to netstandard2.1
    - to target lower framework version

--- src/Sharprinter/PrinterOptions.cs
+++ src/Sharprinter/PrinterOptions.cs
@@ -8,26 +8,26 @@ public sealed class PrinterOptions
     /// <summary>
     ///     Gets or sets the maximum number of characters per line for text wrapping and formatting.
     /// </summary>
-    public int MaxLineCharacter { get; init; }
+    public int MaxLineCharacter { get; set; }
 
     /// <summary>
     ///     Gets or sets the name of the communication port to connect to the printer.
     /// </summary>
-    public string PortName { get; init; } = string.Empty;
+    public string PortName { get; set; } = string.Empty;
 
     /// <summary>
     ///     Gets or sets the baud rate for serial communication with the printer.
     ///     Default value is 9600.
     /// </summary>
-    public int BaudRate { get; init; } = 9600;
+    public int BaudRate { get; set; } = 9600;
 
     /// <summary>
     ///     Gets or sets a value indicating whether the cash drawer should be opened after printing.
     /// </summary>
-    public bool OpenDrawer { get; init; }
+    public bool OpenDrawer { get; set; }
 
     /// <summary>
     ///     Gets or sets a value indicating whether the paper should be cut after printing.
     /// </summary>
-    public bool CutPaper { get; init; }
+    public bool CutPaper { get; set; }
 }
